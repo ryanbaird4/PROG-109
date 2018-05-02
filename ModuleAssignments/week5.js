61ef8e0294ebf5c9cb9332f6c4543769165aa9f4
Ryan Baird <ryanbaird4@gmail.com>
2018-05-02 14:39:39 -0700

    mod 5-1

---
+++ ModuleAssignments/week5.js
@@ -0,0 +1,10 @@
+alert("Welcome User!")
+
+var firstName = prompt("Please enter your first name: ");
+var lastName = prompt("Please enter your last name: ");
+var message = firstName + " " + lastName;
+
+
+document.write('<h3>' + message + '</h3>');
+
+
