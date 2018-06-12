ee209e2b1e13af817855888e4bb02dd3ae02226b
Ryan Baird <ryanbaird4@gmail.com>
2018-06-11 21:10:39 -0700

    edited contact me

---
+++ ContactMe/contactme.js
@@ -0,0 +1,34 @@
+function ValidateForm(){
+var validName = false;
+var validEmail = false;
+var validComment = false;
+
+var letters = /^[A-Za-z]+$/;
+
+var errorMessages ="";  
+
+if (myContact.name.value===null ||
+ myContact.name.value=== "" ||
+    !myContact.name.value.match(letters))
+ errorMessages += "<p>The name is invalid. It must be filled in and consist of letters.</p>";
+ else
+ validName = true; 
+
+if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=email.length) 
+    // send error message. For example: 
+  errorMessages += "<p>Invalid email.</p>";
+else
+    validEmail = true; 
+
+if (myContact.comment.value===null ||
+ myContact.comment.value=== "" ||
+    !myContact.comment.value.match(letters))
+ errorMessages += "<p>The comment is invalid. It must be filled in and consist of letters.</p>";
+ else
+ validComment = true; 
+
+document.getElementById("errorMessages").innerHTML = errorMessages;
+  return (validName && validEmail && validComment) ;
+}
+var d = new Date();
+document.getElementById("date").innerHTML = d;
